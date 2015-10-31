1cc27df8b113821c109ffaa4758be89d87dbbda7
Angel Kurten <angelkurten@hotmail.com>
2015-10-31 00:03:15 -0500

    add helper asset_theme

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,12 @@ function theme_path($theme = 'default')
     return $theme;
 }
 
-function view_path($view)
+function view_path($view, $theme = 'default')
 {
-    return theme_path() . ".views." . $view;
+    return theme_path($theme) . ".views." . $view;
+}
+
+function asset_theme($resource, $theme = 'default')
+{
+    return asset('themes/' . $theme . '/'. $resource);
 }
