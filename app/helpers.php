b6f3634477da2c5af65a58a6eab15f5ce0d34c91
angelkurten <ana6chiu>
2016-03-18 13:50:24 -0500

    Fix case sensitive Ubuntu

--- app/helpers.php
+++ app/helpers.php
@@ -1,16 +1,30 @@
 <?php
 
-function theme_path($theme = 'default')
+function theme_path($theme = null)
 {
+    if(is_null($theme))
+    {
+        $theme = ucfirst(config('themes.default'));
+    }
+
     return $theme;
 }
 
-function view_path($view, $theme = 'default')
+function view_path($view, $theme = null)
 {
+    if(is_null($theme))
+    {
+        $theme = ucfirst(config('themes.default'));
+    }
     return theme_path($theme) . ".views." . $view;
 }
 
-function asset_theme($resource, $theme = 'default')
+function asset_theme($resource, $theme = null)
 {
+    if(is_null($theme))
+    {
+        $theme = config('themes.default');
+    }
+
     return asset('themes/' . $theme . '/'. $resource);
 }
